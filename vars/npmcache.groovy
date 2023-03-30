8661b464abb84f38d84d862ce682ddddd150aa2c
Afaqalam sheik <afaqalam.sheik@hcl.com>
2023-03-31 00:52:40 +0530

    mn

--- vars/npmcache.groovy
+++ vars/npmcache.groovy
@@ -6,7 +6,7 @@ def call(Map config) {
     String nodeModulesDir = "${env.WORKSPACE}/node_modules"
     //String packageJson = readFile("${env.WORKSPACE}/package.json")
     //String packageLockJson = readFile("${env.WORKSPACE}/package-lock.json")
-    String checksum = ChecksumUtils.getChecksum(package.json, package-lock.json)
+    String checksum = ChecksumUtils.getChecksum("package.json", "package-lock.json")
     String cacheKey = "npm-ci-cache-${checksum}"
     String bucketName = "gs://my-new-bucket-12344321-kaushal"
 
