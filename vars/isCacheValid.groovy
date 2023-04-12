1ea92d0dd9adbbbafea5d386a8fae4aa96903076
Afaqalam sheik <afaqalam.sheik@hcl.com>
2023-04-12 13:55:30 +0530

    new-a

--- vars/isCacheValid.groovy
+++ vars/isCacheValid.groovy
@@ -7,21 +7,23 @@ def call(Map config) {
         String bucketName = "gs://${env.JENKINS_GCS_BUCKET}"
         String checksum = ChecksumUtils.getChecksum("${env.WORKSPACE}/package.json", "${env.WORKSPACE}/package-lock.json")
 
-        sh "gsutil stat ${bucketName}/npm-ci-cache-${checksum}"
+        def fileExist = sh(script: "gsutil stat ${bucketName}/npm-ci-cache-${checksum}", returnStatus: true) as Integer
+        //sh "gsutil stat ${bucketName}/npm-ci-cache-${checksum}"
         //sh "gsutil cp ${bucketName}/npm-ci-cache-checksum ."
         //cacheChecksum = readFile('npm-ci-cache-checksum').trim()
+        if (fileExist == 0) {
         echo "Both values cacheChecksum: ${cacheChecksum}, checksum: ${checksum}"
         //sh "echo ${checksum} > npm-ci-cache-checksum"
     	//sh "gsutil cp npm-ci-cache-${checksum} ${bucketName}"
         //if (cacheChecksum == checksum) {
             log('DEBUG', "Cache hit! Skipping npm-ci.")
             return true
-        //} 
-        //else {
-        //    log('DEBUG', "Cache miss! Running npm-ci.")
-	//    sh "gsutil cp npm-ci-cache-${checksum} ${bucketName}"
-        //    return false
-        // } 
+        } 
+        else {
+            log('DEBUG', "Cache miss! Running npm-ci.")
+	        sh "gsutil cp npm-ci-cache-${checksum} ${bucketName}"
+            return false
+         } 
     } catch (Exception e) {
         log('DEBUG', "Error checking cache validity: ${e}")
 	    sh "gsutil cp npm-ci-cache-${checksum} ${bucketName}"
